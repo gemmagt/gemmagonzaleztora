591ed945f57b371e3924b7784def24bf56ae11bd
Gemma <gemma.gonzaleztora@gmail.com>
2022-04-02 10:11:28 +0100

    add publications folder and links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Introduction from "./components/introduction";
 import Gallery from "./components/gallery"
+import Publications from "./components/publications"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Introduction/>
+      <Publications/>
       <Gallery/>
     </div>
   );
