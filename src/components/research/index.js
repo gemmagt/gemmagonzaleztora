503ee44fdab1032b6b2e57601c95c888421227d5
Gemma <gemma.gonzaleztora@gmail.com>
2022-09-04 18:25:26 +0100

    add research tab

---
+++ src/components/research/index.js
@@ -0,0 +1,19 @@
+import "./index.css";
+import { Row, Col} from 'antd';
+
+function Research() {
+  return ( 
+    <>
+      <Row justify="center">
+        <Col span={24}>
+          <div className="research-title">
+           Here is my Cv
+          </div>      
+        </Col>
+      </Row>
+
+    </>
+  );
+}
+
+export default Research;
